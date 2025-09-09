f4f101e5de0ca7008b67dbe305765ee11e4f9c30
Diana <dianaandra19th@hotmail.com>
2025-09-09 19:21:41 +0200

    chore: normalize line endings vis .gitattributes

--- wp-content/themes/planty/functions.php
+++
@@ -1,5 +0,0 @@
-<?php
-add_action('wp_enqueue_scripts', function () {
-  wp_enqueue_style('astra-parent', get_template_directory_uri() . '/style.css');
-  wp_enqueue_style('planty-child', get_stylesheet_uri(), ['astra-parent'], '1.0');
-});
